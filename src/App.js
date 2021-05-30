20e23ff3ab878aad0a7cce1a98ad79b533cadfa2
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-29 21:40:48 -0500

    another commit with more ui elements

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="container">
+      
       <div className="headerLeft">
-        <div className="circle">
+        
+        <div className="sidebar__header">
+        <div className="circle">1</div>
+            <div className="sidebar__headerRight"></div>
 
-        </div>
+          </div>
       </div>
       <div className="headerRight">
-        Header Right
+        <div className="headerRight__container">
+          <div className="headerRight__container1">container1</div>
+          <div className="headerRight__container2">container2</div>
+          <div className="headerRight__container3">container3</div>
+          <div className="headerRight__container4">container4</div>
+          <div className="headerRight__container5">container5</div>
+        </div>
+       
       </div>
     </div>
   );
