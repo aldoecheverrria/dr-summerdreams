9396d75f11217e23d1fcb705d879dea73784b58f
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-06-02 20:39:55 -0500

    advancing within left container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
+import YouTube from '@material-ui/icons/YouTube';
+import Instagram from '@material-ui/icons/Instagram';
+import Facebook from '@material-ui/icons/Facebook';
+import Twitter from '@material-ui/icons/Twitter';
 
 function App() {
   return (
     <div className="container">
       
       <div className="headerLeft">
-        
-        <div className="sidebar__header">
-        <div className="circle">1</div>
-            <div className="sidebar__headerRight"></div>
-
+        <div className="headerLeft__upper">
+          <div className="logo">
+            DreamSpace
           </div>
+          <div className="icons">
+            <Instagram/>
+            <YouTube/>
+            <Facebook/>
+            <Twitter/>
+          </div>
+        </div>
+        <div className="texts">
+          <div className="text1">New Collection</div>
+          <div className="text2">2021</div>
+        </div>
+        <div className="textmiddle">
+          <div className="textmiddle__left">SUMMER</div>
+          <div className="textmiddle__right">DREAMS</div>
+        </div>
+      
       </div>
+
+
       <div className="headerRight">
         <div className="headerRight__container">
           <div className="headerRight__container1">container1</div>
@@ -27,3 +47,17 @@ function App() {
 }
 
 export default App;
+
+
+/*
+<div className="headerLeft__upper" >
+          <div className="logo" >DreamSpace</div>
+          <div className="icons" >
+            <Instagram/>
+            <YouTube/>
+            <Facebook/>
+            <Twitter/>
+          </div>
+        </div>
+
+*/
